2c0ed77a317459dc9c082ae03c8f970c14571d18
AynazFlex <jkz1713x2@gmail.com>
2023-05-05 13:27:04 +0300

    fix about

--- js/main.js
+++ js/main.js
@@ -3,29 +3,33 @@
 const nav = document.body.querySelector(".nav");
 const burger = nav.querySelector(".nav__burger");
 const links = nav.querySelectorAll(".nav__links>li>a");
+const about = document.body.querySelector(".about__container");
+const about_items = about.querySelector(".about__items");
+const about_forward = about.querySelector(".about__forward");
+const about_back = about.querySelector(".about__back");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   nav.classList.toggle("nav__close");
   nav.classList.toggle("open");
-  document.body.style.overflow === "auto" ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto";
+  document.body.style.overflow === "auto"
+    ? (document.body.style.overflow = "hidden")
+    : (document.body.style.overflow = "auto");
 });
 
 links.forEach((item) => {
   item.addEventListener("click", (e) => {
     e.preventDefault();
-    if(e.currentTarget.hash) {
-      document.body
-        .querySelector(e.currentTarget.hash)
-        .scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-          inline: "nearest",
-        });
-        burger.classList.remove("active");
-        nav.classList.add("nav__close");
-        nav.classList.remove("open");
-        document.body.style.overflow = "auto"
+    if (e.currentTarget.hash) {
+      document.body.querySelector(e.currentTarget.hash).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
+      burger.classList.remove("active");
+      nav.classList.add("nav__close");
+      nav.classList.remove("open");
+      document.body.style.overflow = "auto";
     }
   });
 });
@@ -38,89 +42,141 @@ window.addEventListener("scroll", () => {
   }, 200);
 });
 
-const sliderTarget = document.body.querySelector(".works__swiper")
+about_forward.onclick = () => {
+  about_items.scrollBy({
+    left: 300,
+    behavior: 'smooth'
+  })
+}
+
+about_back.onclick = () => {
+  about_items.scrollBy({
+    left: -300,
+    behavior: 'smooth'
+  })
+}
+
+const sliderTarget = document.body.querySelector(".works__swiper");
 
 class Slider {
-    constructor({init, next, prev, slides_class}) {
-        this.init = init;
-        this.next = next;
-        this.prev = prev;
-        // this.activeId = 0;
-        // this.prevId = 0;
-        this.length = this.init.querySelectorAll(`.${slides_class}`).length
-        this.slides_class = slides_class
-    }
+  constructor({ init, next, prev, slides_class }) {
+    this.init = init;
+    this.next = next;
+    this.prev = prev;
+    // this.activeId = 0;
+    // this.prevId = 0;
+    this.length = this.init.querySelectorAll(`.${slides_class}`).length;
+    this.slides_class = slides_class;
+  }
 
-    // _move(index) {
-    //     const width = this.init.getBoundingClientRect().width
-    //     this.activeId = index
-    //     this.init.scrollTo({
-    //         left: index*width,
-    //         behavior: "smooth",
-    //     })
+  // _move(index) {
+  //     const width = this.init.getBoundingClientRect().width
+  //     this.activeId = index
+  //     this.init.scrollTo({
+  //         left: index*width,
+  //         behavior: "smooth",
+  //     })
+  // }
+
+  _anim(elem_1, elem_2) {
+    return Promise.all([
+      new Promise(resolve => {
+        elem_1.addEventListener('animationend', ({currentTarget}) => {
+          currentTarget.style = null;
+          resolve()
+        }, {
+          once: true,
+        })
+      }),
+      new Promise(resolve => {
+        elem_2.addEventListener('animationend', ({currentTarget}) => {
+          currentTarget.style = null;
+          resolve()
+        }, {
+          once: true,
+        })
+      })
+    ])
+  }
+
+  start() {
+    this.next.onclick = () => {
+      // if(this.activeId < this.length-1) {
+      //     this._move(this.prevId + 1)
+      // }
+      const prev = this.init.querySelectorAll(`.${this.slides_class}`)[0];
+      const next = this.init.querySelectorAll(`.${this.slides_class}`)[1];
+      prev.style.animation = "next_1 1000ms ease";
+      next.style.animation = "next_2 1000ms ease";
+      this._anim(prev, next).then(() => this.init.append(prev))
+    };
+
+    this.prev.onclick = () => {
+      // if(this.activeId > 0) {
+      //     this._move(this.prevId - 1)
+      // }
+      this.init.prepend(
+        this.init.querySelectorAll(`.${this.slides_class}`)[this.length - 1]
+      );
+      const prev = this.init.querySelectorAll(`.${this.slides_class}`)[0];
+      const next = this.init.querySelectorAll(`.${this.slides_class}`)[1];
+      prev.style.animation = "prev_2 1000ms ease";
+      next.style.animation = "prev_1 1000ms ease";
+      this._anim(prev, next)
+    };
+
+    // this.init.onscroll = () => {
+    //     clearTimeout(window.scrollEndTimer)
+    //     window.scrollEndTimer = setTimeout(() => {
+    //       this.prevId = this.activeId
+    //     }, 100)
     // }
 
-    start() {
-        this.next.onclick = () => {
-            // if(this.activeId < this.length-1) {
-            //     this._move(this.prevId + 1)
-            // }
-            this.init.style.animation = "move 200ms linear"
-            this.init.append(this.init.querySelectorAll(`.${this.slides_class}`)[0])
-        }
-
-        this.prev.onclick = () => {
-            // if(this.activeId > 0) {
-            //     this._move(this.prevId - 1)
-            // }
-            this.init.style.animation = "move 200ms linear"
-            this.init.prepend(this.init.querySelectorAll(`.${this.slides_class}`)[this.length - 1])
-        }
-
-        // this.init.onscroll = () => {
-        //     clearTimeout(window.scrollEndTimer)
-        //     window.scrollEndTimer = setTimeout(() => {
-        //       this.prevId = this.activeId
-        //     }, 100)
-        // }
-
-        this.init.onanimationend = ({currentTarget}) => {
-          currentTarget.style = null
-        }
-
-        // window.onresize = () => {
-        //     this.init.scrollTo({
-        //         left: this.init.getBoundingClientRect().width*this.prevId
-        //     })
-        // }
-    }
+    // window.onresize = () => {
+    //     this.init.scrollTo({
+    //         left: this.init.getBoundingClientRect().width*this.prevId
+    //     })
+    // }
+  }
 }
 
 const slider = new Slider({
-    init: sliderTarget.querySelector(".works__swiper-container"),
-    next: sliderTarget.querySelector(".works__swiper-next"),
-    prev: sliderTarget.querySelector(".works__swiper-prev"),
-    slides_class: 'works__swiper-item'
-})
+  init: sliderTarget.querySelector(".works__swiper-container"),
+  next: sliderTarget.querySelector(".works__swiper-next"),
+  prev: sliderTarget.querySelector(".works__swiper-prev"),
+  slides_class: "works__swiper-item",
+});
 
-slider.start()
+slider.start();
 
-sliderTarget.onclick = ({target}) => {
-  if(target.matches('.works__swiper-item_img>button')) {
-    target.closest('.works__swiper-item_img').querySelector('.photos__swiper').classList.remove('close')
-    document.body.style.overflow = "hidden"
+sliderTarget.onclick = ({ target }) => {
+  if (target.matches(".works__swiper-item_img>button")) {
+    target
+      .closest(".works__swiper-item_img")
+      .querySelector(".photos__swiper")
+      .classList.remove("close");
+    document.body.style.overflow = "hidden";
   }
-  if(target.closest('.photos__swiper-close')) {
-    target.closest('.works__swiper-item_img').querySelector('.photos__swiper').classList.add('close')
-    document.body.style.overflow = "auto"
+  if (
+    target.closest(".photos__swiper") &&
+    (target.closest(".photos__swiper-close") ||
+      (!target.closest(".photos__swiper-container") &&
+        !target.closest(".photos__swiper-prev") &&
+        !target.closest(".photos__swiper-next")))
+  ) {
+    target
+      .closest(".works__swiper-item_img")
+      .querySelector(".photos__swiper")
+      .classList.add("close");
+    document.body.style.overflow = "auto";
   }
-}
+};
 
-sliderTarget.querySelectorAll('.works__swiper-item').forEach(item => {
+sliderTarget.querySelectorAll(".works__swiper-item").forEach((item) => {
   new Slider({
     init: item.querySelector(".photos__swiper-container"),
     next: item.querySelector(".photos__swiper-next"),
     prev: item.querySelector(".photos__swiper-prev"),
-    slides_class: 'photos__swiper-item'
-  }).start()
-})
+    slides_class: "photos__swiper-item",
+  }).start();
+});
